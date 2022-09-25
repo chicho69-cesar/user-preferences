a13ebd4614db7d49dd2071f3c3ad68b990a51f7f
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-09-24 23:34:46 -0500

    Basic design added

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:user_preferences/widgets/widgets.dart';
+
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
@@ -11,8 +13,65 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: const Center(
-        child: Text('SettingsScreen'),
+      drawer: const SideMenu(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Settings',
+                style: TextStyle(fontSize: 45, fontWeight: FontWeight.w300),
+              ),
+
+              const Divider(),
+
+              SwitchListTile(
+                value: true, 
+                title: const Text('Darkmode'),
+                onChanged: (value) {
+
+                }
+              ),
+
+              const Divider(),
+
+              RadioListTile<int>(
+                value: 1, // 1 - Masculino y 2 - Femenino
+                groupValue: 1, 
+                title: const Text('Masculino'),
+                onChanged: (value) {
+
+                }
+              ),
+
+              const Divider(),
+
+              RadioListTile<int>(
+                value: 2, // 1 - Masculino y 2 - Femenino
+                groupValue: 1, 
+                title: const Text('Femenino'),
+                onChanged: (value) {
+
+                }
+              ),
+
+              const Divider(),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  initialValue: 'Cesar',
+                  decoration: const InputDecoration(
+                    labelText: 'Nombre',
+                    helperText: 'Nombre del usuario'
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
